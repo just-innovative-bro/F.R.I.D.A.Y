930c0cb65e3bc1365a72248cb63180a27babea5f
Mohammed Zameel ali <55248408+Zameel-Byte@users.noreply.github.com>
2023-01-04 19:39:35 +0530

    Update Friday.py
    Solved issue BAN-B605

--- Friday.py
+++ Friday.py
@@ -1,4 +1,4 @@
-import os
+import os, shlex
 import sys
 from gtts import gTTS  # pip install gTTS
 import requests
@@ -213,7 +213,7 @@ def read_note():
 
 def cmd():
     """opens command promt"""
-    os.system("start cmd")
+    os.system(shlex.quote(cmd))
 
 
 def ip():
